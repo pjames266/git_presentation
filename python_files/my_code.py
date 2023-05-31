7d57d143c22ad5cfc1f481a7b779b60e231d02d3
James Price <pjames266@gmail.com>
2023-05-31 13:40:22 -0700

    fixing my bugs

--- python_files/my_code.py
+++ python_files/my_code.py
@@ -12,12 +12,30 @@ class Truck:
         """
         self.make = make
         self.year = year
+        self.name = "Truck"
 
-    def __print__(self):
+    def __init__(self, make, year, name):
+        """truck constructor
+
+        Args:
+            make (string): make/model of truck
+            year (int): year of truck release
+            name (string): name of truck
+        """
+        self.make = make
+        self.year = year
+        self.name = name
+
+    def __str__(self):
+        """_summary_
+
+        Returns:
+            String: String to print when called in print
+        """
         
-        return "I am a truck"
+        return "My name is " + self.name
 
     
-def __main__():
-    optimus_prime = Truck("Peterbilt 379", 1992)
+if __name__ == '__main__':
+    optimus_prime = Truck("Peterbilt 379", 1992, "Optimus Prime")
     print(optimus_prime)
